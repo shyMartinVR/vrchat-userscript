ebb9730c00acf28bb56358fa0001b1b07396c527
shyMartin <shyMartinVR@gmail.com>
2024-04-08 15:34:26 +0200

    added global vars for easy console access

--- src/data.ts
+++ src/data.ts
@@ -6,3 +6,12 @@ export const users: Map<UserID, User> = new Map();
 export const friends: Map<UserID, Friend> = new Map();
 export const avatars: Map<AvatarID, Avatar> = new Map();
 export const worlds: Map<WorldID, World> = new Map();
+
+// @ts-ignore
+unsafeWindow.users = users;
+// @ts-ignore
+unsafeWindow.friends = friends;
+// @ts-ignore
+unsafeWindow.avatars = avatars;
+// @ts-ignore
+unsafeWindow.worlds = worlds;
