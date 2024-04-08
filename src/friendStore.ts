e8e2e270c5fe2e9762ff6625a2556a316e753b0e
shyMartin <shyMartinVR@gmail.com>
2024-04-08 19:27:07 +0200

    moved friendstore to indexedDB

--- src/friendStore.ts
+++ src/friendStore.ts
@@ -1,39 +1,63 @@
-import { Friend, UserID } from "../types/User";
+import { UserID } from "../types/User";
 import { friends } from "./data";
 import { waitForElement } from "./global";
 import { CustomProxy } from "./proxy";
 
-export default function run(){
-CustomProxy.Listen(
-  ({ subPath }) => subPath === "auth/user/friends",
-  (_) => {
-    waitForElement(".ps-1").then((elem) => {
-      if (friends.size === parseInt(elem.textContent)) {
-        const prevFriends: UserID[] = JSON.parse(localStorage.getItem("friends")) || [];
-        localStorage.setItem(
-          "friends",
-          JSON.stringify(Array.from(friends.keys()))
-        );
-        if (prevFriends) {
-          for (const friend of prevFriends) {
-            if (!friends.has(friend)) {
-              console.log(
-                `Missing friend: https://vrchat.com/home/user/${friend}`
-              );
-              elem.parentElement.append("❗");
-              const missingFriends = new Set(
-                JSON.parse(localStorage.getItem("missingFriends"))
-              );
-              missingFriends.add(friend);
-              localStorage.setItem(
-                "missingFriends",
-                JSON.stringify(Array.from(missingFriends))
-              );
-            }
+export default function run() {
+  const friendStore = "friends";
+  const dbRequest = indexedDB.open("martin", 2);
+  dbRequest.onupgradeneeded = (_) => {
+    if (!dbRequest.result.objectStoreNames.contains(friendStore))
+      dbRequest.result.createObjectStore(friendStore);
+  };
+  const dbPromise: Promise<IDBDatabase> = new Promise((resolve, reject) => {
+    dbRequest.onsuccess = (_) => resolve(dbRequest.result);
+    dbRequest.onerror = reject;
+  });
+
+  const elemPromise = waitForElement(".ps-1");
+  const allFriendsPromise = new Promise<void>((resolve, _) => {
+    CustomProxy.Listen(
+      ({ subPath }) => subPath === "auth/user/friends",
+      (_) => {
+        elemPromise.then((elem) => {
+          if (friends.size === parseInt(elem.textContent)) resolve();
+        });
+      }
+    );
+  });
+
+  Promise.all([elemPromise, dbPromise, allFriendsPromise]).then(
+    ([elem, db, _]) => {
+      for (const [key, friend] of friends) {
+        db.transaction(friendStore, "readwrite")
+          .objectStore(friendStore)
+          .put({ id: key, name: friend.displayName }, key);
+      }
+
+      db
+        .transaction(friendStore, "readonly")
+        .objectStore(friendStore)
+        .getAll().onsuccess = function (event) {
+        const prevFriends: { id: UserID; name: string }[] = this.result;
+
+        for (const { id, name } of prevFriends) {
+          if (!friends.has(id)) {
+            console.log(
+              `Missing friend ${name} https://vrchat.com/home/user/${id}`
+            );
+            const button = document.createElement("button");
+            button.textContent = name;
+            button.onclick = (event) => {
+              db
+                .transaction(friendStore, "readwrite")
+                .objectStore(friendStore)
+                .delete(id).onsuccess = () => button.remove();
+            };
+            elem.parentElement.parentElement.parentElement.append(button);
           }
         }
-      }
-    });
-  }
-);
+      };
+    }
+  );
 }
